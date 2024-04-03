fe5138c2235354b2bf901ab7e1fcd8df767e753a
Gabriela-Cassoli <gabrielajorna@gmail.com>
2024-04-03 09:16:33 -0300

    site

--- app.py
+++ app.py
@@ -1,1 +1,20 @@
+import requests
+from flask import Flask, request, render_template
 
+app=Flask(__name__)
+
+@app.route("/home")
+def home():
+  return render_template('home.html')
+
+@app.route("/sobremim")
+def sobremim():
+  return render_template('sobremim.html')
+  
+@app.route("/portfolio")
+def portfolio():
+  return render_template('portfolio.html')
+
+@app.route("/contato")
+def contato():
+  return render_template('contato.html')
