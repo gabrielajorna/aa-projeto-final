9f2c3b268d7e2da29320f3266dfdd58b2d579b5a
Gabriela-Cassoli <gabrielajorna@gmail.com>
2024-04-05 16:19:27 -0300

    incluindo scraping

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
 from flask import Flask, render_template, url_for
+from scraping import raspar_insper
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:55.0) Gecko/20100101 Firefox/55.0',
+}
+url_insper = "https://www.insper.edu.br/imprensa/"
 
 app=Flask(__name__)
 
@@ -17,3 +23,27 @@ def portfolio():
 @app.route("/contato")
 def contato():
   return render_template('contato.html')
+
+@app.route("/noticias")
+def noticias():
+  html = """
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>Notícias Insper</title>
+    </head>
+    <body>
+        <h1>Fique por dentro</h1>
+        <p>
+        Confira as últimas notícias da instituição que trabalho - Insper:
+        <ul>
+    """
+  for materia in raspar_insper(headers, url_insper):
+    html+= f'<li> <a href="{materia["link_insper"]}">{materia["titulo"]}</a> </li>'
+    html+= """
+        </ul>
+        </p>
+    </body>
+    </html>
+    """
+    return html
---
+++ scraping.py
@@ -0,0 +1,20 @@
+import requests
+import bs4
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:55.0) Gecko/20100101 Firefox/55.0',
+}
+url_insper = "https://www.insper.edu.br/imprensa/"
+
+def raspar_insper(headers, url_insper):
+  result_insper = requests.get(url_insper,  headers = headers)
+  soup_insper = bs4.BeautifulSoup(result_insper.text, 'html.parser')
+  links_insper = soup_insper.find_all('div', { 'class': '_thumbnail__descricao no-paddingM' })
+  noticias_insper = []
+  for link in links_insper:
+    link_insper = link.find('a').get('href')
+    titulo = link.find('a').text.strip()
+    noticias_insper.append([titulo,link_insper])
+  return noticias_insper
+
+raspar_insper(headers, url_insper)
